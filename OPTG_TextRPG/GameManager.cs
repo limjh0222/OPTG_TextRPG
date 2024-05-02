cb0db04860b7d8f5ef6d4cccfacc466b68d84936
Kyr001 <cuppastorm@gmail.com>
2024-05-02 12:25:25 +0900

    Add Store.cs
    GameMananger의 상점 코드를 Store.cs로 옮겼습니다

--- OPTG_TextRPG/GameManager.cs
+++ OPTG_TextRPG/GameManager.cs
@@ -20,9 +20,9 @@ public class GameManager
 
     public static GameManager Instance { get { return instance; } }
 
-    private Player player;
+    public Player player;
     public List<Item> inventory {  get; set; }
-    private List<Item> storeInventory;
+    public List<Item> storeInventory;
 
     public GameManager()
     {
@@ -100,7 +100,7 @@ public class GameManager
                 Inventory.InventoryMenu();
                 break;
             case 3:
-                StoreMenu();
+                Store.StoreMenu();
                 break;
         }
         MainMenu();
@@ -140,103 +140,6 @@ public class GameManager
         {
             case 0:
                 MainMenu();
-                break;
-        }
-    }
-
-    private void StoreMenu()
-    {
-        int choice = -1;
-        while(choice < 0 )
-        {
-            Console.Clear();
-
-            ConsoleUtility.PrintMagenta("■ 상점 ■");
-            ConsoleUtility.PrintYellowHighlights("필요한 아이템을 얻을 수 있는 ", "상점", "입니다.\n");
-
-            Console.WriteLine("\n[보유 골드]");
-            ConsoleUtility.PrintYellowHighlights("", player.Gold.ToString(), " G\n");
-
-            Console.WriteLine("\n[아이템 목록]");
-            for (int i = 0; i < storeInventory.Count; i++)
-            {
-                storeInventory[i].PrintStoreItemDescription();
-            }
-
-            Console.WriteLine("\n1. 아이템 구매");
-            Console.WriteLine("0. 나가기\n");
-
-            choice = ConsoleUtility.PromptMenuChoice(0, 1);
-        }
-        
-        switch (choice)
-        {
-            case 0:
-                MainMenu();
-                break;
-            case 1:
-                PurchaseMenu();
-                break;
-        }
-    }
-
-    private void PurchaseMenu(string? prompt = null)
-    {
-        if (prompt != null)
-        {
-            // 1초간 메시지를 띄운 다음에 다시 진행
-            Console.Clear();
-            ConsoleUtility.PrintMagenta(prompt);
-            Thread.Sleep(1000);
-        }
-
-        int choice = -1;
-        while(choice < 0)
-        {
-            Console.Clear();
-
-            ConsoleUtility.PrintMagenta("■ 상점 ■");
-            ConsoleUtility.PrintYellowHighlights("필요한 아이템을 얻을 수 있는 ", "상점", "입니다.\n");
-
-            Console.WriteLine("\n[보유 골드]");
-            ConsoleUtility.PrintYellowHighlights("", player.Gold.ToString(), " G\n");
-
-            Console.WriteLine("\n[아이템 목록]");
-            for (int i = 0; i < storeInventory.Count; i++)
-            {
-                storeInventory[i].PrintStoreItemDescription(true, i + 1);
-            }
-
-            Console.WriteLine("\n0. 나가기\n");
-
-            choice = ConsoleUtility.PromptMenuChoice(0, storeInventory.Count);
-        }
-        
-
-        switch (choice)
-        {
-            case 0:
-                StoreMenu();
-                break;
-            default:
-                // 1 : 이미 구매한 경우
-                if (storeInventory[choice - 1].IsPurchased) // index 맞추기
-                {
-                    PurchaseMenu("이미 구매한 아이템입니다.");
-                }
-                // 2 : 돈이 충분해서 살 수 있는 경우
-                else if(player.Gold >= storeInventory[choice - 1].Price)
-                {
-                    player.Gold -= storeInventory[choice - 1].Price;
-                    storeInventory[choice - 1].Purchase();
-                    inventory.Add(storeInventory[choice - 1]);
-                    PurchaseMenu();
-                }
-                // 3 : 돈이 모자라는 경우
-                else
-                {
-                    PurchaseMenu("Gold가 부족합니다.");
-                }
                 break;
         }
     }
---
+++ OPTG_TextRPG/Store.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPTG_TextRPG
+{
+    internal class Store
+    {
+        public static void StoreMenu()
+        {
+            int choice = -1;
+            while (choice < 0)
+            {
+                Console.Clear();
+
+                ConsoleUtility.PrintMagenta("■ 상점 ■");
+                ConsoleUtility.PrintYellowHighlights("필요한 아이템을 얻을 수 있는 ", "상점", "입니다.\n");
+
+                Console.WriteLine("\n[보유 골드]");
+                ConsoleUtility.PrintYellowHighlights("", GameManager.Instance.player.Gold.ToString(), " G\n");
+
+                Console.WriteLine("\n[아이템 목록]");
+                for (int i = 0; i < GameManager.Instance.storeInventory.Count; i++)
+                {
+                    GameManager.Instance.storeInventory[i].PrintStoreItemDescription();
+                }
+
+                Console.WriteLine("\n1. 아이템 구매");
+                Console.WriteLine("0. 나가기\n");
+
+                choice = ConsoleUtility.PromptMenuChoice(0, 1);
+            }
+
+            switch (choice)
+            {
+                case 0:
+                    GameManager.Instance.MainMenu();
+                    break;
+                case 1:
+                    PurchaseMenu();
+                    break;
+            }
+        }
+
+        private static void PurchaseMenu(string? prompt = null)
+        {
+            if (prompt != null)
+            {
+                // 1초간 메시지를 띄운 다음에 다시 진행
+                Console.Clear();
+                ConsoleUtility.PrintMagenta(prompt);
+                Thread.Sleep(1000);
+            }
+
+            int choice = -1;
+            while (choice < 0)
+            {
+                Console.Clear();
+
+                ConsoleUtility.PrintMagenta("■ 상점 ■");
+                ConsoleUtility.PrintYellowHighlights("필요한 아이템을 얻을 수 있는 ", "상점", "입니다.\n");
+
+                Console.WriteLine("\n[보유 골드]");
+                ConsoleUtility.PrintYellowHighlights("", GameManager.Instance.player.Gold.ToString(), " G\n");
+
+                Console.WriteLine("\n[아이템 목록]");
+                for (int i = 0; i < GameManager.Instance.storeInventory.Count; i++)
+                {
+                    GameManager.Instance.storeInventory[i].PrintStoreItemDescription(true, i + 1);
+                }
+
+                Console.WriteLine("\n0. 나가기\n");
+
+                choice = ConsoleUtility.PromptMenuChoice(0, GameManager.Instance.storeInventory.Count);
+            }
+
+
+            switch (choice)
+            {
+                case 0:
+                    StoreMenu();
+                    break;
+                default:
+                    // 1 : 이미 구매한 경우
+                    if (GameManager.Instance.storeInventory[choice - 1].IsPurchased) // index 맞추기
+                    {
+                        PurchaseMenu("이미 구매한 아이템입니다.");
+                    }
+                    // 2 : 돈이 충분해서 살 수 있는 경우
+                    else if (GameManager.Instance.player.Gold >= GameManager.Instance.storeInventory[choice - 1].Price)
+                    {
+                        GameManager.Instance.player.Gold -= GameManager.Instance.storeInventory[choice - 1].Price;
+                        GameManager.Instance.storeInventory[choice - 1].Purchase();
+                        GameManager.Instance.inventory.Add(GameManager.Instance.storeInventory[choice - 1]);
+                        PurchaseMenu();
+                    }
+                    // 3 : 돈이 모자라는 경우
+                    else
+                    {
+                        PurchaseMenu("Gold가 부족합니다.");
+                    }
+                    break;
+            }
+        }
+    }
+}
