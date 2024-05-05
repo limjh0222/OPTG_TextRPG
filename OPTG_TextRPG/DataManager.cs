2b824c529a3d641e85802c50d2384ba5a50b024d
jelly1702 <urin1702@gmail.com>
2024-05-05 13:04:05 +0900

    Refactor DataManager
    아이템 정보 변경

--- OPTG_TextRPG/DataManager.cs
+++ OPTG_TextRPG/DataManager.cs
@@ -74,28 +74,20 @@ class DataManager
     }
     public void InitItem() //아이템타입,공격력,방어력,체력,가격
     {
-        ItemDB.Add(new Item("녹슨 단검", "아무것도 없는것보단 낫지 않겠어요?.", ItemType.WEAPON, 5, 0, 0, 1500));
-        ItemDB.Add(new Item("낡은 클럽", "베는것만이 답이 아닐수도 있죠.", ItemType.WEAPON, 7, 0, 0, 2000));
-        ItemDB.Add(new Item("롱소드", "이제 몬스터와의 싸움은 두렵지않아요.", ItemType.WEAPON, 10, 0, 0, 2300));
-        ItemDB.Add(new Item("메이스", "흔히 철퇴라고 하죠. 맞으면 상당히 아플지도..", ItemType.WEAPON, 13, 0, 0, 3000));
-        //ItemDB.Add(new Item("레이피어", "찌른다. 또 찌른다.. 계속 찌른다!", ItemType.WEAPON, 15, 0, 0, 3800));
-        //ItemDB.Add(new Item("채찍", "혹시.. 그런 취향 이신가요..?", ItemType.WEAPON, 17, 0, 0, 4500));
-        //ItemDB.Add(new Item("스피어", "꼭 가까이서 싸울필요는 없잖아요?", ItemType.WEAPON, 20, 0, 0, 5200));
-        //ItemDB.Add(new Item("배틀엑스", "외형만큼이나 무겁죠. 무겁지만 아파요.", ItemType.WEAPON, 25, 0, 0, 6000));
-        //ItemDB.Add(new Item("클레이모어", "이름만큼 외형도 공격도 일품!", ItemType.WEAPON, 35, 0, 0, 9700));
+        ItemDB.Add(new Item("녹슨 단검", "없는것보단 낫지 않겠어요?.", ItemType.WEAPON, 5, 0, 0, 1300));
+        ItemDB.Add(new Item("낡은 클럽", "베는것만이 답이 아닐수도 있죠.", ItemType.WEAPON, 9, 0, 0, 2000));
+        ItemDB.Add(new Item("롱소드", "이제 몬스터와의 싸움은 두렵지않아요.", ItemType.WEAPON, 13, 0, 0, 2700));
+        ItemDB.Add(new Item("레이피어", "찌른다. 또 찌른다.. 계속 찌른다!", ItemType.WEAPON, 18, 0, 0, 3600));
+        ItemDB.Add(new Item("스피어", "가까이에서 싸울필요는 없잖아요?", ItemType.WEAPON, 23, 0, 0, 4500));
+        ItemDB.Add(new Item("배틀엑스", "외형만큼이나 무겁죠. 무겁지만 아파요.", ItemType.WEAPON, 28, 0, 0, 5800));
+        ItemDB.Add(new Item("클레이모어", "이름만큼 외형도 공격도 일품!", ItemType.WEAPON, 34, 0, 0, 7500));
 
-        ItemDB.Add(new Item("허름한 천옷", "누군가 입었던것 같지만 쓸만할지도..", ItemType.ARMOR, 0, 5, 0, 500));
-        ItemDB.Add(new Item("가죽갑옷", "가죽이지만 장인의 손길로 나름 튼튼하다.", ItemType.ARMOR, 0, 7, 0, 1000));
-        ItemDB.Add(new Item("청동갑옷", "", ItemType.ARMOR, 0, 14, 0, 2000));
-        ItemDB.Add(new Item("판금갑옷", "", ItemType.ARMOR, 0, 17, 0, 2800));
-        //ItemDB.Add(new Item("아머", "튼튼한 갑옷.", ItemType.ARMOR, 0, 6, 0, 3000));
-        //ItemDB.Add(new Item("", "튼튼한 갑옷.", ItemType.ARMOR, 0, 6, 0, 3000));
-        //ItemDB.Add(new Item("풀 메일", "튼튼한 갑옷.", ItemType.ARMOR, 0, 6, 0, 3000));
-        //ItemDB.Add(new Item("미스릴아머", "튼튼한 갑옷.", ItemType.ARMOR, 0, 6, 0, 3000));
-        //ItemDB.Add(new Item("드래곤스킨", "튼튼한 갑옷.", ItemType.ARMOR, 0, 6, 0, 3000));
-
-
-
+        ItemDB.Add(new Item("허름한 조끼", "낡은것 같지만 쓸만할지도?", ItemType.ARMOR, 0, 3, 0, 800));
+        ItemDB.Add(new Item("가죽 갑옷", "장인의 손길로 한땀한땀!.", ItemType.ARMOR, 0, 5, 0, 1500));
+        ItemDB.Add(new Item("브리간딘", "이제 좀 튼튼한거 같아요.", ItemType.ARMOR, 0, 8, 0, 2800));
+        ItemDB.Add(new Item("사슬 갑옷", "촘촘하게 만들어서 튼튼해요.", ItemType.ARMOR, 0, 12, 0, 3700));
+        ItemDB.Add(new Item("미스릴 갑옷", "드워프가 만든건 일품이죠.", ItemType.ARMOR, 0, 15, 0, 5300));
+        ItemDB.Add(new Item("드래곤스킨", "전설로 내려오는 갑옷이에요!", ItemType.ARMOR, 0, 20, 0, 7200));
 
         //ItemDB.Add(new Item("HP 포션", "체력을 회복시켜 주는 물약.", ItemType.PORTION, 0, 0, 10, 100));
         // ItemDB.Add(new Item("MP 포션", "마나를 회복시켜 주는 물약", ItemType.PORTION, 0, 0, 10, 100));
